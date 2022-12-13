ffc1f34633fe54bdc14ee0581d6b67e333a79266
changyi <changyi@biomap.com>
2022-12-13 22:11:54 +0800

    feat: ceshi1

--- packages/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export * as core from './core/index';
-
-export * as web from './web/index';
--- packages/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * as plugin from './plugins/index';
-
-export * as lib from './lib/index';
-
-export * as types from './types/index';
